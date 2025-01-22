7eb3b14f4db7e73c19534ea138c72f11aaab4fec
Riley Webb <web0018@elthamhs.vic.edu.au>
2025-01-22 19:18:14 +1100

    Initial commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <SDL2/SDL.h>
+
+#define SDL_MAIN_HANDLED
+
+int main(int argc, char *argv[])
+{
+    //printf("Hello, World!\n");
+
+    //CL_Init();
+
+    return 0;
+}
